cb066a105263f62237a3507a614a49fb7ad24c84
Filipp Gudis <fgudis@yahoo.com>
2021-01-13 00:39:13 -0800

    hooks completed!

--- src/index.js
+++ src/index.js
@@ -8,17 +8,20 @@ import "./index.css";
 import App from "./App";
 // import productReducer from "./store/reducers/products";
 // import ProductsProvider from './context/products-context';
-import configureProductsStore from './hooks-store/products-store';
+import configureProductsStore from "./hooks-store/products-store";
 
 configureProductsStore();
 
-
 ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-  
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+
   document.getElementById("root")
 );
 // starting project's state is managed using redux store and reducer, actions....
 // we will convert this approach to react hooks!!
+
+// redux is great to use in bigger applications where we have a very large state to manage!
+// context API is great for low frequency rendering chunks of state management
+// custom hooks (hook stores) are great for state management if we dont want to have extra library dependencies, and we want to use react and js only
